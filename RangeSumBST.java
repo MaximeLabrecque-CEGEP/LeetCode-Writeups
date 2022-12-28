8c556b29a6317301da07e950dd13520e6d459a68
Maxime Labrecque <2033605@cstjean.qc.ca>
2022-12-28 09:53:47 -0500

    Create RangeSumBST.java

---
+++ RangeSumBST.java
@@ -0,0 +1,22 @@
+// Challenge #938
+
+
+public class RangeSumBST {
+    public int rangeSum(TreeNode root, int low, int high) {
+
+        return 1;
+    }
+}
+
+class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+     TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
